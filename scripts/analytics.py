ccb7e8429f3de8d515e4f2afc748b72d9a8a5b2d
Xiao <aqxiao@amazon.com>
2025-08-04 11:42:37 -0700

    updated output script

--- scripts/analytics.py
+++ scripts/analytics.py
@@ -4,9 +4,18 @@ import numpy as np
 
 # read file
 query_output_df = pd.read_excel('../aon_deals.xlsx', sheet_name='QUERY OUTPUT')
-df = query_output_df.copy()
+deal_sme_input_df = pd.read_excel('../aon_deals.xlsx', sheet_name='DEAL SME INPUT')
+
+# join dataframes
+df = pd.concat(
+    [query_output_df, deal_sme_input_df],
+    axis=1,
+    join='inner',
+    on=['asin', 'paws_promotion_id']
+)
 
 df.head(3)
+
 # %%
 # discount per unit
 df['discount_per_unit'] = df['asp'] - df['promotion_pricing_amount']
@@ -16,6 +25,7 @@ df['discount_per_unit'] = np.where(
     df['discount_per_unit']
 )
 
+# calculate incremental gain per unit
 df['incremental_per_unit'] = df['total_vendor_funding'] - df['discount_per_unit']
 df['incremental_per_unit'] = np.where(
     (df['incremental_per_unit'] < 0) | (df['incremental_per_unit'].isna()),
@@ -23,7 +33,7 @@ df['incremental_per_unit'] = np.where(
     df['incremental_per_unit']
 )
 
-
+# calculate total incremental gains
 df['incremental_gains'] = df['incremental_per_unit'] * df['shipped_units']
 df['incremental_gains'] = np.where(
     (df['incremental_gains'] < 0) | (df['incremental_gains'].isna()),
@@ -36,5 +46,5 @@ df.head(3)
 
 # %%
 # output
-df.to_excel('../output.xlsx')
+df.to_excel('../aon_deals.xlsx', sheet_name='OUTPUT', index=False)
 # %%
